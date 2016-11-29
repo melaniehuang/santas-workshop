9b2079929514b8637979a0344c955b22ec31f81d
Mel Huang <hello@melaniehuang.com>
2016-11-30 09:14:48 +1100

    looking at different ways to animate sprites

--- scripts/elves.js
+++ scripts/elves.js
@@ -1,14 +1,33 @@
+var mouse_moved = false;
+var touch_started = false;
+
+var elf_sprite_sheet;
+var elf_frames = [
+  {"name":"elf_walk00", "frame":{"x":0, "y": 0, "width": 128, "height": 128}},
+  {"name":"elf_walk01", "frame":{"x":128, "y": 0, "width": 128, "height": 128}},
+  {"name":"elf_walk02", "frame":{"x":256, "y": 0, "width": 128, "height": 128}},
+  {"name":"elf_walk03", "frame":{"x":384, "y": 0, "width": 128, "height": 128}}
+];
 var elf;
+var elf_walk;
+var elf_stand;
+
+function preload() {
+  elf_sprite_sheet = loadSpriteSheet("data/elf_walk.png", elf_frames);
+  elf_walk = loadAnimation(elf_sprite_sheet);
+  elf_stand = loadAnimation(new SpriteSheet("data/elf.png",[
+    {"name":"elf_stand", "frame":{"x":0, "y": 0, "width": 128, "height": 128}}
+  ]));
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  createSprite(400, 200, 50, 50);
-  background(45,21,11);
-  var animatedSprite = loadAnimation("/data/walk00.png","/data/walk01.png","/data/walk02.png","/data/walk03.png");
+  createSprite(0, 0, 10, 10);
+  background(45,21,11);  
   
-  elf = createSprite(windowWidth/2, windowHeight/2, 32, 32);
-  elf.addAnimation("moving", animatedSprite);
-
+  elf = createSprite(windowWidth/2, windowHeight/2, 128, 128);
+  elf.addAnimation('walk', elf_walk);
+  elf.addAnimation('stand', elf_stand);
 }
 
 function draw() {
@@ -16,20 +35,40 @@ function draw() {
   fill(66,43,29);
   rect(20,20,windowWidth-40,windowHeight-40);  
   
-  if(mouseX < elf.position.x - 10) {
-    elf.changeAnimation("moving");
+  var eventX;
+  
+  if (isTouch()) {
+    eventX = touchX;
+  } else {
+    eventX = mouseX;
+  }
+
+  if (eventX < elf.position.x - 10) {
+    elf.changeAnimation('walk');
     elf.mirrorX(-1);
     elf.velocity.x = - 2;
   }
-  else if(mouseX > elf.position.x + 10) {
-    elf.changeAnimation("moving");
+  else if (eventX > elf.position.x + 10) {
+    elf.changeAnimation('walk');
     elf.mirrorX(1);
     elf.velocity.x = 2;
   }
   else {
-    elf.changeAnimation("floating");
+    elf.changeAnimation('stand');
     elf.velocity.x = 0;
   }
 
   drawSprites();
+}
+
+function touchStarted() {
+  touch_started = true;
+}
+
+function mouseMoved() {
+  mouse_moved = true;
+}
+
+function isTouch() {
+  return touch_started && !mouse_moved;
 }
