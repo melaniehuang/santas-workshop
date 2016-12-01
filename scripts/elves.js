65086816e5b67bbfbbc959d8e2fd46fa2fe78d72
Mel Huang <hello@melaniehuang.com>
2016-12-01 22:37:59 +1100

    random generating elves on screen

--- scripts/elves.js
+++ scripts/elves.js
@@ -5,10 +5,15 @@ window.onload = function() {
     update: update, 
     render: render
   });
-  
+
+  var elf;
+  var elves;
+
   var map;
   var layer;
   
+  var yLocation = Array(120, 442, 760);
+
   function preload () {
     scene.load.atlasJSONHash("elf", "/assets/sprites/elf_walk.png", "/assets/sprites/elf_walk.json");
 
@@ -18,16 +23,35 @@ window.onload = function() {
 
   function create () {
     scene.stage.backgroundColor = 0x422B1D;
+    scene.physics.setBoundsToWorld();
 
     map = scene.add.tilemap("map", 80, 80);
     map.addTilesetImage("wood");
     
     layer = map.createLayer(0);
-    layer.resizeWorld(0);
+    layer.resizeWorld();
 
-    var elf = scene.add.sprite(128, 137, "elf");
-    elf.animations.add("run");
-    elf.animations.play("run", 12, true);
+    elves = scene.add.group();
+    elves.enableBody = true;
+    elves.physicsBodyType = Phaser.Physics.ARCADE;
+    
+    for (var y = 0; y < 4; y++){
+      elf = elves.create(0, yLocation[Math.floor(Math.random()*yLocation.length)], "elf");
+      elf.anchor.set(0.5,0.5);
+      elf.animations.add("run");
+      elf.animations.play("run", 12, true);
+
+      elf.checkWorldBounds = true;
+      elf.events.onOutOfBounds.add(elfOut, this);
+      elf.body.velocity.x = Math.floor((Math.random() * 100) + 50);
+    }
+
+  }
+  
+  function elfOut(elf) {
+    elf.reset(elf.x, 0);
+    elf.reset(elf.y, yLocation[Math.floor(Math.random()*yLocation.length)]);
+    elf.body.velocity.x = Math.floor((Math.random() * 100) + 50);
   }
 
   function update () {
@@ -36,3 +60,5 @@ window.onload = function() {
   function render () {
   }
 };
+
+// elf.scale.x = -1;
