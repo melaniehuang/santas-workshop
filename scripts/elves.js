8ecfc8b67d98c116a28a0c0b0f4f427d2ed8ba9b
Mel Huang <hello@melaniehuang.com>
2016-12-05 23:27:18 +1100

    added new sprites

--- scripts/elves.js
+++ scripts/elves.js
@@ -12,14 +12,17 @@ window.onload = function() {
   var map;
   var layer;
   
-  var yLocation = Array(40, 280, 520, 760);
-  var spriteImages = Array("elf", "elf2", "elf3", "elf4");
+  var yLocation = Array(118, 280, 440, 600, 760);
+  var spriteImages = Array("elf", "elf2", "elf3", "elf4", "elf5", "elf6", "elf7");
 
   function preload () {
-    scene.load.atlasJSONHash("elf", "/assets/sprites/elf_walk.png", "/assets/sprites/elf_walk.json");
-    scene.load.atlasJSONHash("elf2", "/assets/sprites/elf_walk2.png", "/assets/sprites/elf_walk.json");
-    scene.load.atlasJSONHash("elf3", "/assets/sprites/elf_carry.png", "/assets/sprites/elf_walk.json");
-    scene.load.atlasJSONHash("elf4", "/assets/sprites/elf_overhead.png", "/assets/sprites/elf_walk.json");
+    scene.load.atlasJSONHash("elf", "/assets/sprites/elf-walk.png", "/assets/sprites/elf_walk.json");
+    scene.load.atlasJSONHash("elf2", "/assets/sprites/elf-walk-red.png", "/assets/sprites/elf_walk.json");
+    scene.load.atlasJSONHash("elf3", "/assets/sprites/elf-carry.png", "/assets/sprites/elf_walk.json");
+    scene.load.atlasJSONHash("elf4", "/assets/sprites/elf-carry-skateboard.png", "/assets/sprites/elf_walk.json");
+    scene.load.atlasJSONHash("elf5", "/assets/sprites/elf-overhead-present-blue.png", "/assets/sprites/elf_walk.json");
+    scene.load.atlasJSONHash("elf6", "/assets/sprites/elf-overhead-candycane.png", "/assets/sprites/elf_walk.json");
+    scene.load.atlasJSONHash("elf7", "/assets/sprites/elf-overhead-present-magenta.png", "/assets/sprites/elf_walk.json");
 
     scene.load.tilemap("map", "assets/tiles/tilemap.csv", null, Phaser.Tilemap.CSV);
     scene.load.image("wood", "assets/tiles/tilemap-wood.png");
@@ -40,11 +43,11 @@ window.onload = function() {
     elves.enableBody = true;
     elves.physicsBodyType = Phaser.Physics.ARCADE;
     
-    for (var y = 0; y < 10; y++){
+    for (var y = 0; y < 8; y++){
       elf = elves.create(0, yLocation[Math.floor(Math.random()*yLocation.length)], spriteImages[Math.floor(Math.random()*spriteImages.length)]);
       elf.anchor.set(0.5,0.5);
       elf.animations.add("run")
-      elf.animations.play("run", 12, true);
+      elf.animations.play("run", 10, true);
 
       elf.checkWorldBounds = true;
       elf.events.onOutOfBounds.add(elfOut, this);
